0c9112dfd5d8c383bb28029dc0f174a78d844344
Anatoliy <targoniyanatoliy@gmail.com>
2024-06-30 12:37:53 +0300

    final version

--- scripts/script.js
+++ scripts/script.js
@@ -8,31 +8,20 @@ function changeClass(e) {
     if (e.target.tagName == 'IMG') {
         const item = e.target.closest('.gallery-item');
         const gallery = item.parentElement;
+
         for (let i = 0; i < gallery.children.length; i++) {
             if (gallery.children[i].classList.contains('active')) {
                 gallery.children[i].classList.remove('active');
             }
         }
+
         item.classList.add('active');
+
         if (gallery.className === 'testimonials-gallery') {
             gallery.prepend(item);
         }
     }
 }
-
-
-
-testimonialsGallery.addEventListener('click', (e) => {
-    if (e.target.tagName == 'IMG') {
-        for (let i = 0; i < gallery1.children.length; i++) {
-            if (gallery1.children[i].classList.contains('active')) {
-                gallery1.children[i].classList.remove('active');
-            }
-        }
-        e.target.closest('.gallery-item').classList.add('active');
-    }
-});
-
 
 let partners = document.querySelector('.partners-row');
 let g2row2 = document.querySelector('.gallery-row2');
